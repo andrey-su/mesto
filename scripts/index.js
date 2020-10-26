5531f6bb126fe5f49cc5b8d2b0c78bba80e995db
Andrey Suryakov <an.suryakov@yandex.ru>
2020-10-26 17:36:29 +0300

    проектная работа после первого код-ревью

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,19 @@ let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__button-close');
 let buttonOpenPopup = document.querySelector('.profile__button');
 
+//переменные для написания текста в формах
+let formElement = popup.querySelector('.popup__form');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let inputName = popup.querySelector('.popup__input_type_name');
+let inputActivity = popup.querySelector('.popup__input_type_activity');
+
 
 // функция открытия попапа
 function showPopup() {
-popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
+  inputName.value = profileTitle.textContent;
+  inputActivity.value = profileSubtitle.textContent;
 /*popup.removeEventListener('click', showPopup);*/
 }
 
@@ -15,32 +24,23 @@ buttonOpenPopup.addEventListener('click', showPopup);
   
 // функция закрытия попапа
 function closePopup() {
-popup.classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 // отлежиивание события клика для закрытия popup
 popupCloseButton.addEventListener('click', closePopup);
 
 
-
-//переменные для написания текста в формах
-let formElement = popup.querySelector('.popup__form');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-
-
 //функция  для заммены  текста 
 function formSubmitHandler(event) {
-event.preventDefault();
-closePopup();
+  event.preventDefault();
+  closePopup();
 
-
-let inputName = popup.querySelector('.popup__input_type_name');
-let inputActivity = popup.querySelector('.popup__input_type_activity');
-
-profileTitle.textContent = inputName.value;
-profileSubtitle.textContent = inputActivity.value;
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputActivity.value;
 }
+
+
 
 //обработчик событий
 formElement.addEventListener('submit', formSubmitHandler);
